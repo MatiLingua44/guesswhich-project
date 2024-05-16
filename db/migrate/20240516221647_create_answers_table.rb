3a432fbdef6e366035a32b55693a5ee179b1770a
marcelojuarez <marcelo395juarez@gmail.com>
2024-05-16 19:22:55 -0300

    answers table

---
+++ db/migrate/20240516221647_create_answers_table.rb
@@ -0,0 +1,11 @@
+class CreateAnswersTable < ActiveRecord::Migration[7.1]
+  def change
+    create_table :answers do |t|
+      t.string :description
+      t.boolean :is_correct
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
