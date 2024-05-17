1f162bc71f58574d8a2fe4e9bb80314a274745b2
juancreynoso <juancreynoso11@gmail.com>
2024-05-17 18:48:21 -0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_16_230206) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_205123) do
   create_table "answers", force: :cascade do |t|
     t.string "description"
     t.boolean "is_correct"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_16_230206) do
 
   create_table "questions", force: :cascade do |t|
     t.string "description"
+    t.integer "difficulty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
