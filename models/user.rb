203161ce63c139053f1f86ff77d37fbe0658e0bc
marcelojuarez <marcelo395juarez@gmail.com>
2024-05-17 19:10:24 -0300

    user model modified

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,12 @@
 # models/user.rb
 
 class User < ActiveRecord::Base
+      before_create :set_default_score
+
+      private
+  
+      def set_default_score
+        self.score ||= 0 
+      end
 end
   
