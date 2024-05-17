be988ef5bcb3b72d8a083a7d8e9ebc3bba70014b
juancreynoso <juancreynoso11@gmail.com>
2024-05-17 00:57:20 -0300

    questions table migration

---
+++ db/migrate/20240516230206_create_questions_table.rb
@@ -0,0 +1,10 @@
+class CreateQuestionsTable < ActiveRecord::Migration[7.1]
+  def change
+    create_table :questions do |t|
+      t.string :description
+      #t.integer :difficulty
+
+      t.timestamps
+    end
+  end
+end
