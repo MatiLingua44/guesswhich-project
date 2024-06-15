b55bff143ff517c79fc85a098b9e2b1c8622e416
marcelojuarez <marcelo395juarez@gmail.com>
2024-06-15 16:07:34 -0300

    server modified

--- server.rb
+++ server.rb
@@ -137,7 +137,7 @@ class App < Sinatra::Application
     post '/events' do
         event = params[:event]
         session[:selected_event] = event
-
+        session[:user_score] = 0
         event_titles = {
           '0' => "Second World War",
           '1' => "Industrial Revolution",
