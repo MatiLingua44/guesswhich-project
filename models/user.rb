d0c6a83e756f55e4a6f07d42accc8caf88e604a6
juancreynoso <juancreynoso11@gmail.com>
2024-06-18 01:56:15 -0300

    user model modified for token managament

--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,22 @@
 # models/user.rb
 
+require 'bcrypt'
+require 'securerandom'
 class User < ActiveRecord::Base
       before_create :set_default_score
+      has_secure_password
 
-      private
+      def generate_password_reset_token!
+        self.password_reset_token = SecureRandom.urlsafe_base64
+        self.password_reset_sent_at = Time.now.utc
+        save!
+      end
+
+      def clear_password_reset_token!
+        self.password_reset_token = nil
+        self.password_reset_sent_at = nil
+        save!
+      end
   
       def set_default_score
         self.score ||= 0 
